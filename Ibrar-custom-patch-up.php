06b781bc0245f11d328a9f83719a800d9a7cc179
haxdar <hasnainsardar8@gmail.com>
2023-09-15 08:44:46 +0100

    Added the option for multiple category selection

--- Ibrar-custom-patch-up.php
+++ Ibrar-custom-patch-up.php
@@ -25,32 +25,36 @@ if(! defined ('ABSPATH')){
     die;
 }
 
-// Add the settings page 
-function custom_patch_ups_select_the_category_settings_page() {
+
+// Add the settings page
+function custom_patch_ups_select_the_category_settings_page()
+{
     add_options_page('Custom Patch UPs Settings', 'Custom Patch UPs', 'manage_options', 'custom-patch-ups-settings', 'custom_patch_ups_exclude_posts_settings_form');
 }
 add_action('admin_menu', 'custom_patch_ups_select_the_category_settings_page');
 
-// Settings for to select the category
-function custom_patch_ups_exclude_posts_settings_form() {
+// Settings for selecting multiple categories
+function custom_patch_ups_exclude_posts_settings_form()
+{
     ?>
     <div class="wrap">
-        <h2>Custom Patch UPs Settings</h2>
+        <h2>Custom Patch UP Settings</h2>
         <form method="post" action="options.php">
             <?php settings_fields('custom_patch_ups_settings'); ?>
             <?php do_settings_sections('custom-patch-ups-settings'); ?>
             <table class="form-table">
                 <tr valign="top">
-                    <th scope="row">Select Category to Exclude Posts from Home Page</th>
+                    <th scope="row">Select Categories to Exclude Posts from Home Page</th>
                     <td>
                         <?php
-                        $selected_category = get_option('custom_patch_ups_category');
-                        wp_dropdown_categories(array(
-                            'show_option_none' => 'Select a category',
-                            'name' => 'custom_patch_ups_category',
-                            'orderby' => 'name',
-                            'selected' => $selected_category,
-                        ));
+                        $selected_categories = get_option('custom_patch_ups_categories', array());
+
+                        $all_categories = get_categories();
+
+                        foreach ($all_categories as $category) {
+                            $checked = in_array($category->term_id, (array)$selected_categories) ? 'checked' : '';
+                            echo '<div class="col-md-4"><input type="checkbox" id="category_' . $category->term_id . '" name="custom_patch_ups_categories[]" value="' . $category->term_id . '" ' . $checked . '><label for="category_' . $category->term_id . '">' . $category->name . '</label></div>';
+                        }
                         ?>
                     </td>
                 </tr>
@@ -61,22 +65,23 @@ function custom_patch_ups_exclude_posts_settings_form() {
     <?php
 }
 
-function custom_patch_ups_exclude_posts_register_settings() {
-    register_setting('custom_patch_ups_settings', 'custom_patch_ups_category');
+function custom_patch_ups_exclude_posts_register_settings()
+{
+    register_setting('custom_patch_ups_settings', 'custom_patch_ups_categories');
 }
 add_action('admin_init', 'custom_patch_ups_exclude_posts_register_settings');
 
-
 // Code to exclude the specific category posts from the home page
-function custom_patch_ups_exclude_posts_by_category_from_homepage($query) {
+function custom_patch_ups_exclude_posts_by_category_from_homepage($query)
+{
     $theme = wp_get_theme();
     if ('Astra' == $theme->name || 'Astra' == $theme->parent_theme) {
         if ($query->is_home() && $query->is_main_query()) {
-            $selected_category = get_option('custom_patch_ups_category');
-            if ($selected_category) {
-                $query->set('category__not_in', array($selected_category));
+            $selected_categories = get_option('custom_patch_ups_categories');
+            if ($selected_categories && is_array($selected_categories) && count($selected_categories) > 0) {
+                $query->set('category__not_in', $selected_categories);
             }
         }
     }
 }
-add_action('pre_get_posts', 'custom_patch_ups_exclude_posts_by_category_from_homepage');
+add_action('pre_get_posts_custom_exclude', 'custom_patch_ups_exclude_posts_by_category_from_homepage');
